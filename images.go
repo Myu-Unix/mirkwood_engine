d046d044cedd545db0c989d7be194addc0949b1c
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2021-04-18 18:31:39 +0200

    use `go fmt ./...`

--- images.go
+++ images.go
@@ -1,84 +1,84 @@
 package main
 
 import (
-  "github.com/hajimehoshi/ebiten/v2/ebitenutil"
-  "log"
-  _ "image/png"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	_ "image/png"
+	"log"
 )
 
 func load_images() {
 
-var err error
-      background1Image, _, err = ebitenutil.NewImageFromFile("images/background.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      hideImage, _, err = ebitenutil.NewImageFromFile("images/hide.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      adventurer1Image, _, err = ebitenutil.NewImageFromFile("images/avatar.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      adventurer2Image, _, err = ebitenutil.NewImageFromFile("images/avatar2.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      header1Image, _, err = ebitenutil.NewImageFromFile("images/player1_header2.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      header2Image, _, err = ebitenutil.NewImageFromFile("images/player2_header2.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      enemy1Image, _, err = ebitenutil.NewImageFromFile("images/goblin.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      enemy2Image, _, err = ebitenutil.NewImageFromFile("images/warg.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      enemy3Image, _, err = ebitenutil.NewImageFromFile("images/skeleton64.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      enemy4Image, _, err = ebitenutil.NewImageFromFile("images/skeleton64_axe.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      SplashImage, _, err = ebitenutil.NewImageFromFile("images/splash.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      inventoryImage, _, err = ebitenutil.NewImageFromFile("images/inventory.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      dice20Image, _, err = ebitenutil.NewImageFromFile("images/dice20.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      dice4Image, _, err = ebitenutil.NewImageFromFile("images/dice4.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      dice6Image, _, err = ebitenutil.NewImageFromFile("images/dice6.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      dice8Image, _, err = ebitenutil.NewImageFromFile("images/dice8.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      dmImage, _, err = ebitenutil.NewImageFromFile("images/dm.png")
-        if err != nil {
-                log.Fatal(err)
-        }
-      notificationImage, _, err = ebitenutil.NewImageFromFile("images/notification.png")
-        if err != nil {
-                log.Fatal(err)
-        }
+	var err error
+	background1Image, _, err = ebitenutil.NewImageFromFile("images/background.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hideImage, _, err = ebitenutil.NewImageFromFile("images/hide.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	adventurer1Image, _, err = ebitenutil.NewImageFromFile("images/avatar.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	adventurer2Image, _, err = ebitenutil.NewImageFromFile("images/avatar2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	header1Image, _, err = ebitenutil.NewImageFromFile("images/player1_header2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	header2Image, _, err = ebitenutil.NewImageFromFile("images/player2_header2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	enemy1Image, _, err = ebitenutil.NewImageFromFile("images/goblin.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	enemy2Image, _, err = ebitenutil.NewImageFromFile("images/warg.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	enemy3Image, _, err = ebitenutil.NewImageFromFile("images/skeleton64.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	enemy4Image, _, err = ebitenutil.NewImageFromFile("images/skeleton64_axe.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	SplashImage, _, err = ebitenutil.NewImageFromFile("images/splash.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	inventoryImage, _, err = ebitenutil.NewImageFromFile("images/inventory.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dice20Image, _, err = ebitenutil.NewImageFromFile("images/dice20.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dice4Image, _, err = ebitenutil.NewImageFromFile("images/dice4.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dice6Image, _, err = ebitenutil.NewImageFromFile("images/dice6.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dice8Image, _, err = ebitenutil.NewImageFromFile("images/dice8.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dmImage, _, err = ebitenutil.NewImageFromFile("images/dm.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	notificationImage, _, err = ebitenutil.NewImageFromFile("images/notification.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- states.go
+++ states.go
@@ -1,188 +1,188 @@
 package main
 
 import (
-  "github.com/hajimehoshi/ebiten/v2"
-  "math/rand"
-  "os"
-  "time"
+	"github.com/hajimehoshi/ebiten/v2"
+	"math/rand"
+	"os"
+	"time"
 )
 
 func state_handler() {
-     // Move selected player
-       if IsKeyTriggered(ebiten.KeyW) == true {
-         player[STATE_PLAYER_SELECTED-1].posy -= 70
-       }
-       if IsKeyTriggered(ebiten.KeyS) == true {
-        player[STATE_PLAYER_SELECTED-1].posy += 70
-       }
-       if IsKeyTriggered(ebiten.KeyA) == true {
-        player[STATE_PLAYER_SELECTED-1].posx -= 70
-       }
-       if IsKeyTriggered(ebiten.KeyD) == true {
-        player[STATE_PLAYER_SELECTED-1].posx += 70
-       }
-    // Move selected enemy
-       if IsKeyTriggered(ebiten.KeyUp) == true {
-        npc[STATE_ENEMY_SELECTED-1].posy -= 70
-       }
-       if IsKeyTriggered(ebiten.KeyDown) == true {
-        npc[STATE_ENEMY_SELECTED-1].posy += 70
-       }
-       if IsKeyTriggered(ebiten.KeyLeft) == true {
-        npc[STATE_ENEMY_SELECTED-1].posx -= 70
-       }
-       if IsKeyTriggered(ebiten.KeyRight) == true {
-        npc[STATE_ENEMY_SELECTED-1].posx += 70
-       }
-    // Toogle fullscreen
-       if IsKeyTriggered(ebiten.KeyF) == true {
-        if STATE_FULLSCREEN == 0 {
-         ebiten.SetFullscreen(true)
-         STATE_FULLSCREEN=1
-        } else {
-          ebiten.SetFullscreen(false)
-          STATE_FULLSCREEN=0
-        }
-       }
-    // Player choice
-       if IsKeyTriggered(ebiten.KeyP) == true {
-        STATE_SHOW_SPLASH = 0
-        header_posx = 0
-        go click_sound()
-        if STATE_PLAYER_SELECTED < 2 {
-        STATE_PLAYER_SELECTED += 1
-        } else {
-          STATE_PLAYER_SELECTED=1
-         }
-       }
-    // DM screen
-       if IsKeyTriggered(ebiten.KeyU) == true {
-        go click_sound()
-       if STATE_DM == 1 {
-        STATE_DM=0
-       } else {
-        STATE_DM=1
-       }
-       }
-    // Link/Measure
-       if IsKeyTriggered(ebiten.KeyL) == true {
-         go click_sound()
-         if STATE_LINK == 1 {
-           STATE_LINK=0
-         } else {
-           STATE_LINK=1
-         }
-       }
-    // Select enemy
-       if IsKeyTriggered(ebiten.KeyE) == true {
-        go click_sound()
-        if STATE_ENEMY_SELECTED < 4 {
-          STATE_ENEMY_SELECTED += 1
-        } else {
-          STATE_ENEMY_SELECTED = 1
-        }
-      }
-    // Show some debug info
-       if IsKeyTriggered(ebiten.KeyG) == true {
-        go click_sound()
-        if STATE_SHOW_DEBUG == 1 {
-          STATE_SHOW_DEBUG = 0
-        } else {
-          STATE_SHOW_DEBUG = 1
-        }
-       }
-    // Toogle inventory
-       if IsKeyTriggered(ebiten.KeyI) == true {
-         go click_sound()
-         header_posx = 0
-         if STATE_SHOW_INVENTORY == 1 {
-          STATE_SHOW_INVENTORY = 0
-         } else {
-          STATE_SHOW_INVENTORY = 1
-         }
-       }
-    // Quit
-       if IsKeyTriggered(ebiten.KeyK) == true {
-         os.Exit(0)
-       }
+	// Move selected player
+	if IsKeyTriggered(ebiten.KeyW) == true {
+		player[STATE_PLAYER_SELECTED-1].posy -= 70
+	}
+	if IsKeyTriggered(ebiten.KeyS) == true {
+		player[STATE_PLAYER_SELECTED-1].posy += 70
+	}
+	if IsKeyTriggered(ebiten.KeyA) == true {
+		player[STATE_PLAYER_SELECTED-1].posx -= 70
+	}
+	if IsKeyTriggered(ebiten.KeyD) == true {
+		player[STATE_PLAYER_SELECTED-1].posx += 70
+	}
+	// Move selected enemy
+	if IsKeyTriggered(ebiten.KeyUp) == true {
+		npc[STATE_ENEMY_SELECTED-1].posy -= 70
+	}
+	if IsKeyTriggered(ebiten.KeyDown) == true {
+		npc[STATE_ENEMY_SELECTED-1].posy += 70
+	}
+	if IsKeyTriggered(ebiten.KeyLeft) == true {
+		npc[STATE_ENEMY_SELECTED-1].posx -= 70
+	}
+	if IsKeyTriggered(ebiten.KeyRight) == true {
+		npc[STATE_ENEMY_SELECTED-1].posx += 70
+	}
+	// Toogle fullscreen
+	if IsKeyTriggered(ebiten.KeyF) == true {
+		if STATE_FULLSCREEN == 0 {
+			ebiten.SetFullscreen(true)
+			STATE_FULLSCREEN = 1
+		} else {
+			ebiten.SetFullscreen(false)
+			STATE_FULLSCREEN = 0
+		}
+	}
+	// Player choice
+	if IsKeyTriggered(ebiten.KeyP) == true {
+		STATE_SHOW_SPLASH = 0
+		header_posx = 0
+		go click_sound()
+		if STATE_PLAYER_SELECTED < 2 {
+			STATE_PLAYER_SELECTED += 1
+		} else {
+			STATE_PLAYER_SELECTED = 1
+		}
+	}
+	// DM screen
+	if IsKeyTriggered(ebiten.KeyU) == true {
+		go click_sound()
+		if STATE_DM == 1 {
+			STATE_DM = 0
+		} else {
+			STATE_DM = 1
+		}
+	}
+	// Link/Measure
+	if IsKeyTriggered(ebiten.KeyL) == true {
+		go click_sound()
+		if STATE_LINK == 1 {
+			STATE_LINK = 0
+		} else {
+			STATE_LINK = 1
+		}
+	}
+	// Select enemy
+	if IsKeyTriggered(ebiten.KeyE) == true {
+		go click_sound()
+		if STATE_ENEMY_SELECTED < 4 {
+			STATE_ENEMY_SELECTED += 1
+		} else {
+			STATE_ENEMY_SELECTED = 1
+		}
+	}
+	// Show some debug info
+	if IsKeyTriggered(ebiten.KeyG) == true {
+		go click_sound()
+		if STATE_SHOW_DEBUG == 1 {
+			STATE_SHOW_DEBUG = 0
+		} else {
+			STATE_SHOW_DEBUG = 1
+		}
+	}
+	// Toogle inventory
+	if IsKeyTriggered(ebiten.KeyI) == true {
+		go click_sound()
+		header_posx = 0
+		if STATE_SHOW_INVENTORY == 1 {
+			STATE_SHOW_INVENTORY = 0
+		} else {
+			STATE_SHOW_INVENTORY = 1
+		}
+	}
+	// Quit
+	if IsKeyTriggered(ebiten.KeyK) == true {
+		os.Exit(0)
+	}
 
-    // Hidden area on the map
-       if IsKeyTriggered(ebiten.KeyH) == true {
-         STATE_HIDDEN=0
-       }
-    // Kill enemy (temporary)
-       if IsKeyTriggered(ebiten.KeyKP1) == true {
-         npc[0].alive = 0
-         STATE_LINK=0
-       }
-       if IsKeyTriggered(ebiten.KeyKP2) == true {
-         npc[1].alive = 0
-         STATE_LINK=0
-       }
-       if IsKeyTriggered(ebiten.KeyKP3) == true {
-         npc[2].alive = 0
-         STATE_LINK=0
-       }
-       if IsKeyTriggered(ebiten.KeyKP4) == true {
-         npc[3].alive = 0
-         STATE_LINK=0
-       }
-    // Remove health from enemies WIP
-       /*if IsKeyTriggered(ebiten.KeyMinus) == true {
-         npc[STATE_ENEMY_SELECTED].hp_max = strconv(npc[STATE_ENEMY_SELECTED].hp_max) -= 1
-       } */
-       
-    // Change game round
-       if IsKeyTriggered(ebiten.KeyN) == true {
-        notification_posx = 1920 
-         go click_sound()
-         if STATE_ROUND < 2 {
-          STATE_ROUND +=1
-         } else {
-          STATE_ROUND = 0
-         }
-       }
-    // Dices be rollin'
-       if IsKeyTriggered(ebiten.KeyR) == true { // roll dices
-        go dice_sound()
-        time.Sleep(80 * time.Millisecond)
-        s1 := rand.NewSource(time.Now().UnixNano())
-        r1 := rand.New(s1)
-        time.Sleep(80 * time.Millisecond)
-        s2 := rand.NewSource(time.Now().UnixNano())
-        r2 := rand.New(s2)
-        time.Sleep(80 * time.Millisecond)
-        s3 := rand.NewSource(time.Now().UnixNano())
-        r3 := rand.New(s3)
-        time.Sleep(80 * time.Millisecond)
-        s4 := rand.NewSource(time.Now().UnixNano())
-        r4 := rand.New(s4)
-        DICE_20_1 = r1.Intn(20) + 1
-        DICE_4_1 = r2.Intn(4) + 1
-        DICE_6_1 = r3.Intn(6) + 1
-        DICE_8_1 = r4.Intn(8) + 1
-     }
+	// Hidden area on the map
+	if IsKeyTriggered(ebiten.KeyH) == true {
+		STATE_HIDDEN = 0
+	}
+	// Kill enemy (temporary)
+	if IsKeyTriggered(ebiten.KeyKP1) == true {
+		npc[0].alive = 0
+		STATE_LINK = 0
+	}
+	if IsKeyTriggered(ebiten.KeyKP2) == true {
+		npc[1].alive = 0
+		STATE_LINK = 0
+	}
+	if IsKeyTriggered(ebiten.KeyKP3) == true {
+		npc[2].alive = 0
+		STATE_LINK = 0
+	}
+	if IsKeyTriggered(ebiten.KeyKP4) == true {
+		npc[3].alive = 0
+		STATE_LINK = 0
+	}
+	// Remove health from enemies WIP
+	/*if IsKeyTriggered(ebiten.KeyMinus) == true {
+	  npc[STATE_ENEMY_SELECTED].hp_max = strconv(npc[STATE_ENEMY_SELECTED].hp_max) -= 1
+	} */
 
-       // Next map - Disabled
-       /*if IsKeyTriggered(ebiten.KeyN) == true {
-         go click_sound()
-         header_posx = 0
-         STATE_MAP=2
-         npc[0].alive=1
-         npc[1].alive=1
-         npc[2].alive=1
-         npc[3].alive=1
-         player[0].posx = 240
-         player[0].posy = 250
-         player[1].posx = 180
-         player[1].posy = 340
-         npc[0].posx = 1240
-         npc[0].posy = 650
-         npc[1].posx = 1180
-         npc[1].posy = 840  
-         npc[2].posx = 1240
-         npc[2].posy = 250
-         npc[3].posx = 1580
-         npc[3].posy = 340  
-       } */
+	// Change game round
+	if IsKeyTriggered(ebiten.KeyN) == true {
+		notification_posx = 1920
+		go click_sound()
+		if STATE_ROUND < 2 {
+			STATE_ROUND += 1
+		} else {
+			STATE_ROUND = 0
+		}
+	}
+	// Dices be rollin'
+	if IsKeyTriggered(ebiten.KeyR) == true { // roll dices
+		go dice_sound()
+		time.Sleep(80 * time.Millisecond)
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		time.Sleep(80 * time.Millisecond)
+		s2 := rand.NewSource(time.Now().UnixNano())
+		r2 := rand.New(s2)
+		time.Sleep(80 * time.Millisecond)
+		s3 := rand.NewSource(time.Now().UnixNano())
+		r3 := rand.New(s3)
+		time.Sleep(80 * time.Millisecond)
+		s4 := rand.NewSource(time.Now().UnixNano())
+		r4 := rand.New(s4)
+		DICE_20_1 = r1.Intn(20) + 1
+		DICE_4_1 = r2.Intn(4) + 1
+		DICE_6_1 = r3.Intn(6) + 1
+		DICE_8_1 = r4.Intn(8) + 1
+	}
+
+	// Next map - Disabled
+	/*if IsKeyTriggered(ebiten.KeyN) == true {
+	  go click_sound()
+	  header_posx = 0
+	  STATE_MAP=2
+	  npc[0].alive=1
+	  npc[1].alive=1
+	  npc[2].alive=1
+	  npc[3].alive=1
+	  player[0].posx = 240
+	  player[0].posy = 250
+	  player[1].posx = 180
+	  player[1].posy = 340
+	  npc[0].posx = 1240
+	  npc[0].posy = 650
+	  npc[1].posx = 1180
+	  npc[1].posy = 840
+	  npc[2].posx = 1240
+	  npc[2].posy = 250
+	  npc[3].posx = 1580
+	  npc[3].posy = 340
+	} */
 }
